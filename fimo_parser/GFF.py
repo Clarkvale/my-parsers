fd4051c3aa9015c7df581c0f13d2c41eb479c86a
Benjamin <benjamin_r.clark@live.com>
2020-02-10 00:49:48 -0500

    Added header and edited name column to pull using regex

--- fimo_parser/GFF.py
+++ fimo_parser/GFF.py
@@ -8,17 +8,14 @@ Created on Tue Jan 14 14:38:50 2020
 import pandas as pd
 import re
 
-
-
-
 class GFF_Parse:
     
     def __tokenize(self, line):
         line_list = line.split("\t")
-
+        
         line_dict = {}
         
-        line_dict["name"] = line_list[0]
+        line_dict["header"] = line_list[0]
         
         line_dict["start"] = int(line_list[3])
         
@@ -30,17 +27,25 @@ class GFF_Parse:
         
         line_dict["motif_id"] = self.rx_dict["motif_id"].findall(line_list[8])[0]
         
-        line_dict["qvalue"] = float(self.rx_dict["qvalue"].findall(line_list[8])[0])
+        line_dict["name"] = self.rx_dict["name"].findall(line_list[8])[0]
         
-         
         try:
            
             line_dict["pvalue"] = float(self.rx_dict["pvalue"].findall(line_list[8])[0])
-        
+            
         except IndexError:
             
             if "pvalue" not in line_dict.keys():
-                line_dict["pvalue"] = float(self.rx_dict["sci_pval"].findall(line_list[8])[0])
+                line_dict["pvalue"] = float(self.rx_dict["psci_val"].findall(line_list[8])[0])
+            
+        try:
+             line_dict["qvalue"] = float(self.rx_dict["qvalue"].findall(line_list[8])[0])
+             
+        except IndexError:
+             
+             if "qvalue" not in line_dict.keys():
+                line_dict["qvalue"] = float(self.rx_dict["qsci_val"].findall(line_list[8])[0])
+                
             
         
         return line_dict
@@ -69,12 +74,14 @@ class GFF_Parse:
         self.rx_dict = {"pvalue" : re.compile(";pvalue=(0.\d+);"),
                     "qvalue": re.compile(";qvalue=(0.\d+);"),
                     "sequence": re.compile(";sequence=(\w+);"),
-                    "sci_pval" : re.compile(";pvalue=(\d\.\d+e-\d+);"),
-                    "motif_id": re.compile("ID=([^-]+)")}
+                    "psci_val" : re.compile(";pvalue=(\d\.?\d*e-\d+);"),
+                    "motif_id": re.compile("ID=([^-]+)"),
+                    "qsci_val" : re.compile(";qvalue=(\d\.?\d*e-\d+);"),
+                    "name" : re.compile("Name=[^_]*_([^;+-]*)")}
         
 
         self.dataframe = pd.DataFrame(self.__parse(self.path))
-        self.dataframe = self.dataframe.astype({"name": str})
+        self.dataframe = self.dataframe.astype({"header": str})
         
         
 if __name__ == "__main__":
